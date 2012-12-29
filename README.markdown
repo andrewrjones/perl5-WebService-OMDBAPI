e01bdabfe214dc3aacd8d5befa92f1cfaa0e9a46
Andrew Jones <andrew@arjones.co.uk>
2012-12-29 18:01:41 +0000

    initial commit

---
+++ README.markdown
@@ -0,0 +1,6 @@
+WebService::OMDB
+================
+
+This is a Perl5 interface to http://www.omdbapi.com/.
+
+For more information, see https://metacpan.org/module/WebService::OMDB
---
+++ lib/WebService/OMDB.pm
@@ -0,0 +1,21 @@
+use strict;
+use warnings;
+
+package WebService::OMDB;
+
+# ABSTRACT: Interface to http://www.omdbapi.com/
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+  # TODO
+
+=head1 DESCRIPTION
+
+TODO
+
+=cut
+
